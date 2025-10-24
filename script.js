1f5c93a8b37b02a7c9a9289b8a05e3ba84d852dc
mattskg023 <matthiasmunnikchrontsiou@gmail.com>
2025-10-24 20:43:41 +0200

    Create script.js
    Thank you melding

---
+++ script.js
@@ -0,0 +1,4 @@
+// Kleine interactie: laat een bedankmelding zien bij het versturen van het formulier
+document.querySelector('form').addEventListener('submit', function() {
+  alert("Thank you! Your idea has been submitted.");
+});
